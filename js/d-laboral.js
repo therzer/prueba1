00a2fc685a094d765d46e61ed5930cdada1acea6
therzer <60276551+therzer@users.noreply.github.com>
2022-06-02 10:58:11 -0300

    Add files via upload

---
+++ js/d-laboral.js
@@ -0,0 +1,47 @@
+window.sr = ScrollReveal();
+	sr.reveal('.barra_navegacion', {
+		duration: 2000,
+		origin: 'bottom',
+		distance: '-100px'
+	});
+
+	sr.reveal('.fondo__top', {
+		duration: 3000,
+		origin: 'bottom',
+		distance: '-100px'
+	});
+
+	sr.reveal('.text_top', {
+		duration: 4000,
+		origin: 'bottom',
+		distance: '-100px'
+	});
+
+	sr.reveal('.targetas', {
+		duration: 3000,
+		origin: 'bottom',
+		distance: '400px'
+	});
+
+	sr.reveal('.articulo__despido', {
+		duration: 3000,
+		origin: 'bottom',
+		distance: '-400px'
+	});
+
+	sr.reveal('.titulo', {
+		duration: 4000,
+		origin: 'left',
+		distance: '300px'
+	});
+	sr.reveal('.parrafo', {
+		duration: 5000,
+		origin: 'left',
+		distance: '300px'
+	});
+
+	sr.reveal('.pie_pagina', {
+		duration: 3000,
+		origin: 'bottom',
+		distance: '-400px'
+	});
